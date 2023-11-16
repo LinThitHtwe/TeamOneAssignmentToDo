3be92f88b6ba71759829567a2cc094d9b7ee6e1f
LinnHtet29 <linnh3064@gmail.com>
2023-11-16 23:31:00 +0630

    [add] initial users and login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Login from "./pages/Login";
 import Factorial from "./pages/Factorial";
 import ToDo from "./pages/ToDo";
 import Navbar from "./components/Navbar";
 import { useState } from "react";
 
 function App() {
+  localStorage.setItem(
+    "USER_DB",
+    JSON.stringify([
+      {
+        email: "mario@gmail.com",
+        password: "mario",
+      },
+      {
+        email: "onigiri@gmail.com",
+        password: "onigiri",
+      },
+      {
+        email: "bunbun@gmail.com",
+        password: "bunbun",
+      },
+    ])
+  );
+
+  const [isValid, setValid] = useState(null);
+
+  const validateUserHandler = (email, password) => {
+  const users = JSON.parse(localStorage.getItem("USER_DB"));
+  let isValided = false;
+
+    users.forEach((user) => {
+      if (user.email === email && user.password === password) {
+        isValided = true;
+        return;
+      }
+    });
+
+    if (isValided) {
+      setValid(true);
+    } else {
+      setValid(false);
+    }
+  };
+
+  const clearMsg = () => {
+    setValid(null);
+  };
+
   const [activeComponent, setActiveComponent] = useState("ToDo");
 
   const handleNavbarClick = (component) => {
@@ -14,6 +57,28 @@ function App() {
 
   return (
     <>
+      {!isValid && (
+        <Login validateUserHandler={validateUserHandler} clearMsg={clearMsg} />
+      )}
+      {isValid === false && (
+        <p
+          style={{
+            width: "60vw",
+            padding: "20px",
+            fontSize: "1.3rem",
+            background: "#FF2E2E",
+            margin: "auto",
+            color: "white",
+            position: "absolute",
+            top: "20px",
+            left: "0",
+            right: "0",
+            zIndex: "1000",
+          }}
+        >
+          Invalid email or password
+        </p>
+      )}
       <Navbar
         onNavbarClick={handleNavbarClick}
         activeComponent={activeComponent}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,67 @@
-import React from "react";
+import { useState } from "react";
+import "../styles/login.css";
 
-const Login = () => {
+export default function Login({ validateUserHandler, clearMsg }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const changeEmailHandler = (event) => {
+    setEmail(event.target.value);
+    clearMsg();
+  };
+
+  const changePasswordHandler = (event) => {
+    setPassword(event.target.value);
+    clearMsg();
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    validateUserHandler(email, password);
+  };
+
   return (
-    <div>
-      <h1>Login Init</h1>
-    </div>
+    <main className="login-container">
+      <section className="left container">
+        <img
+          className="image"
+          src={process.env.PUBLIC_URL + "/undraw_team_up_re_84ok.svg"}
+          alt=""
+        />
+      </section>
+      <section className="right container">
+        <form onSubmit={(event) => submitHandler(event)} id="login-form" action="#">
+          <h2>Admin Sign In</h2>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={changeEmailHandler}
+              type="email"
+              id="email"
+              value={email}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={changePasswordHandler}
+              type="password"
+              id="password"
+              value={password}
+            />
+          </div>
+          <div id="remember-me-con">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+          <div id="button-con">
+            <button type="submit">Login</button>
+          </div>
+          <a href="#" id="forgot-password">
+            Forgot password
+          </a>
+        </form>
+      </section>
+    </main>
   );
-};
-
-export default Login;
+}
