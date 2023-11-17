66ec4c5b90f41c6d8863709ff377cee4d2b0cfe2
HeinMinLatt1235 <123471377+HeinMinLatt1235@users.noreply.github.com>
2023-11-17 10:09:50 +0630

    [add] even-odd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Factorial from './pages/Factorial';
+import EvenOdd from './pages/EvenOdd';
 
 function App() {
   return (
-    <>
-      <Factorial />
-    </>
+    <div>
+      <EvenOdd/>
+    </div>
   );
+  
 }
 
 export default App;
